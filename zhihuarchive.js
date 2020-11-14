7e17ded768d02b01e8aae86d8e5ce28726a1e7bb
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2020-11-14 17:44:05 -0500

    Create zhihuarchive.js

---
+++ zhihuarchive.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Zhihu archive
+// @namespace    http://www.zhihu.com/
+// @version      0.1
+// @description  Automatically archive zhihu page 众所周知，知乎内容经常被小管家咔擦。安装本脚本后，访问知乎内容时会自动向http://web.archive.org/申请存档
+// @author       Jinzhe Zeng
+// @match        https://www.zhihu.com/question/*
+// @match        https://www.zhihu.com/question/*/answer/*
+// @match        https://zhuanlan.zhihu.com/p/*
+// @grant       GM_xmlhttpRequest
+// @connect     web.archive.org
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var url = window.location.href;
+    GM_xmlhttpRequest ( {
+    method:     'GET',
+    url:        'http://web.archive.org/save/'+url,
+    onload:     function (responseDetails) {
+        console.log (
+            "Save " + url + " successfully:\n",
+            responseDetails.responseText, '\n',
+            "See http://web.archive.org/web/*/"+url
+        );
+    }
+} );
+})();
