d81d943ecf72ef40e848049155748556c4e8d8ec
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2020-11-15 12:47:24 -0500

    remove logging responseDetails
    fix #4 
    bump version to 0.2

--- zhihuarchive.js
+++ zhihuarchive.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Zhihu archive
 // @namespace    http://www.zhihu.com/
-// @version      0.1
+// @version      0.2
 // @description  Automatically archive zhihu page 众所周知，知乎内容经常被小管家咔擦。安装本脚本后，访问知乎内容时会自动向http://web.archive.org/申请存档
 // @author       Jinzhe Zeng
 // @match        https://www.zhihu.com/question/*
@@ -21,7 +21,6 @@
     onload:     function (responseDetails) {
         console.log (
             "Save " + url + " successfully:\n",
-            responseDetails.responseText, '\n',
             "See http://web.archive.org/web/*/"+url
         );
     }
