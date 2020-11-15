7a0a826c3d55c47947ab6d477a5d760ae8895f73
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2020-11-15 15:33:25 -0500

    use https instead
    bump version to 0.3

--- zhihuarchive.js
+++ zhihuarchive.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Zhihu archive
 // @namespace    http://www.zhihu.com/
-// @version      0.2
-// @description  Automatically archive zhihu page 众所周知，知乎内容经常被小管家咔擦。安装本脚本后，访问知乎内容时会自动向http://web.archive.org/申请存档
+// @version      0.3
+// @description  Automatically archive zhihu page 众所周知，知乎内容经常被小管家咔擦。安装本脚本后，访问知乎内容时会自动向 https://web.archive.org/ 申请存档
 // @author       Jinzhe Zeng
 // @match        https://www.zhihu.com/question/*
 // @match        https://www.zhihu.com/question/*/answer/*
@@ -17,11 +17,11 @@
     var url = window.location.href;
     GM_xmlhttpRequest ( {
     method:     'GET',
-    url:        'http://web.archive.org/save/'+url,
+    url:        'https://web.archive.org/save/'+url,
     onload:     function (responseDetails) {
         console.log (
             "Save " + url + " successfully:\n",
-            "See http://web.archive.org/web/*/"+url
+            "See https://web.archive.org/web/*/"+url
         );
     }
 } );
