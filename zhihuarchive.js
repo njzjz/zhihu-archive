51b7d700d71151439ccbb066d3c6c37e70695366
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-12-06 14:34:17 -0500

    bump version to 0.4

--- zhihuarchive.js
+++ zhihuarchive.js
@@ -1,14 +1,15 @@
 // ==UserScript==
 // @name         Zhihu archive
 // @namespace    http://www.zhihu.com/
-// @version      0.3
-// @description  Automatically archive zhihu page 众所周知，知乎内容经常被小管家咔擦。安装本脚本后，访问知乎内容时会自动向 https://web.archive.org/ 申请存档
+// @version      0.4
+// @description  Automatically archive zhihu page 众所周知，知乎内容经常被小管家咔擦。安装本脚本后，访问知乎内容时会自动向 https://web.archive.org/ 申请存档对应的 www.fxzhihu.com 页面
 // @author       Jinzhe Zeng
 // @match        https://www.zhihu.com/question/*
 // @match        https://www.zhihu.com/question/*/answer/*
 // @match        https://zhuanlan.zhihu.com/p/*
 // @grant       GM_xmlhttpRequest
 // @connect     web.archive.org
+// @license LGPL-3.0-or-later; https://www.gnu.org/licenses/lgpl-3.0.txt
 // ==/UserScript==
 
 (function() {
